8efec0ab6ed0df2fe7960a5f6951a824334b59e6
frederickaquino45-del <frederick.aquino45@gmail.com>
2026-06-30 20:04:32 -0300

    feat: Update SMTP configuration with Gmail settings for email functionality

--- Lightcode.Registration.Application/Configuration/MasterSmtpOptions.cs
+++ Lightcode.Registration.Application/Configuration/MasterSmtpOptions.cs
@@ -4,19 +4,19 @@ public sealed class MasterSmtpOptions
 {
     public const string SectionName = "MasterSmtp";
 
-    public bool UseSmtp { get; set; }
+    public bool UseSmtp { get; set; } = true;
 
-    public string Host { get; set; } = string.Empty;
+    public string Host { get; set; } = "smtp.gmail.com";
 
     public int Port { get; set; } = 587;
 
-    public string Usuario { get; set; } = string.Empty;
+    public string Usuario { get; set; } = "contato.asfsolutions@gmail.com";
 
-    public string Senha { get; set; } = string.Empty;
+    public string Senha { get; set; } = "zurq wvwq rckk xvom";
 
-    public string EmailRemetente { get; set; } = string.Empty;
+    public string EmailRemetente { get; set; } = "contato.asfsolutions@gmail.com";
 
     public string NomeRemetente { get; set; } = "Lightcode";
 
-    public bool UsarSsl { get; set; }
+    public bool UsarSsl { get; set; } = true;
 }
